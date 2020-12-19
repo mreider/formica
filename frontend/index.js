37d9b002e6e454d515c9df8c2fb317c1e9e27bf2
Matthew Reider <matthew.reider@dynatrace.com>
2020-12-19 14:21:46 +0100

    environment vars and dynamic html

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '3001';
+const backend = process.env.BACKEND || 'localhost';
+const backendport = process.env.BACKENDPORT || '3000';
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+app.get('/', (req, res) => {
+    res.render('index.html', { root: __dirname, backend: backend, backendport: backendport });
+});
+app.listen(port, () => console.log(`listening on port ${port}!`));
