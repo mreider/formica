8215ee587bb8a56b347fab1ddd922f290a96a70d
Kulsoom Malik <m.kulsoom22@gmail.com>
2020-12-19 07:17:04 +0500

    rdis uploader

---
+++ backend/redis-data-upload.js
@@ -0,0 +1,19 @@
+const redis = require("redis");
+const { promisify } = require("util");
+var emoji = require('emoji.json');
+
+(async()=>{
+    const client = redis.createClient({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD
+    });
+    client.on("error", (error) => {
+        console.error("error: ", error);
+    });
+
+    console.log("length: ", emoji.length);
+    for(let i=0; i<emoji.length; i++) {
+        client.set(i, emoji[i]);
+    }
+})();
